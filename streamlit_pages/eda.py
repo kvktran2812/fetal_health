9537cf31b40cd964347450863439469185d1f44a
kvktran2812 <kvktran@gmail.com>
2024-11-28 23:34:37 -0500

    eda and pca

--- streamlit_pages/eda.py
+++ streamlit_pages/eda.py
@@ -1,3 +1,20 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("fetal_health.csv")
+
+st.title("Exploratory Data Analysis")
+
+st.write("This page is for exploratory data analysis of the fetal health dataset.")
+
+st.write("First lets look at the data samples and its description")
+st.write(df.head())
+st.write(df.describe())
+
+st.markerdown("""
+The data consists of 2126 rows with 21 features and a target variable 'fetal health'. 
+The features are related to fetal health parameters such as fetal movement, uterine contractions, and accelerations.
+""")
